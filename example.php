37f5856bf5dd3207f40c8b6ef262d249cd091158
Khurram765 <khurramkhalilkhurramkhalil312@gmail.com>
2023-08-18 18:47:14 -0700

    changes in example.php page

--- example.php
+++ example.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor vel rem molestias itaque minima. Eveniet illo deleniti ipsa, expedita, fugit esse tenetur ut dicta nulla quia praesentium harum porro repellendus ipsam corporis nemo officiis minima. Cupiditate odio officia voluptate, voluptas exercitationem rerum veritatis suscipit, minima esse qui recusandae iusto delectus voluptates, dolorum nihil asperiores! Ipsum soluta eaque autem placeat fuga, animi, nihil corrupti atque explicabo error voluptatibus minima pariatur minus adipisci doloremque nesciunt inventore vel numquam? Error repudiandae consequuntur suscipit et blanditiis. Doloremque architecto earum animi inventore laborum iusto quae quaerat at. Corrupti, aut nisi! Vel doloremque aliquam impedit a.</p>
 </body>
 </html>
